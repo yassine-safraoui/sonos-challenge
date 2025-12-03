826d9139a4c97dd3cdccebe2d58c890bde463562
Yassine Safraoui <yassine.safraoui@grenoble-inp.org>
2025-12-03 10:20:58 +0100

    modern folder structure

--- src/audio/source.rs
+++ src/audio/source.rs
@@ -2,7 +2,7 @@ use hound::{Error, WavSpec};
 
 pub trait AudioSource {
     fn get_spec(&self) -> Result<WavSpec, Error>;
-    fn get_samples(&mut self, buf: &mut Vec<i16>) -> Result<(), Error>;
+    fn get_all_samples(&mut self, buf: &mut Vec<i16>) -> Result<(), Error>;
 }
 
 pub struct WavAudioSource {
@@ -14,6 +14,9 @@ impl WavAudioSource {
         let reader = hound::WavReader::open(filepath)?;
         Ok(Self { reader })
     }
+    pub fn iter_samples(&mut self) -> impl Iterator<Item = Result<i16, Error>> + '_ {
+        self.reader.samples::<i16>()
+    }
 }
 
 impl AudioSource for WavAudioSource {
@@ -21,7 +24,7 @@ impl AudioSource for WavAudioSource {
         Ok(self.reader.spec())
     }
 
-    fn get_samples(&mut self, buf: &mut Vec<i16>) -> Result<(), Error> {
+    fn get_all_samples(&mut self, buf: &mut Vec<i16>) -> Result<(), Error> {
         for sample in self.reader.samples::<i16>() {
             buf.push(sample?);
         }
